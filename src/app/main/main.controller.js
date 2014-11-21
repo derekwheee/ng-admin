778f5d31c26371328e38499df5d613b3655b2b47
Derek Wheelden <derek.wheelden@gmail.com>
2014-11-20 22:37:31 -0500

    Adding styles and new elements

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,126 +1,128 @@
 'use strict';
 
-angular.module('ngAdmin')
-    .controller('MainCtrl', function ($scope) {
+angular.module('ngAdmin').controller('MainCtrl', function ($scope) {
 
-        $scope.dashboardPanels = [
-            {
-                'uid'   : '234623471',
-                'title' : 'Pages',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 3'
-                    }
-                ]
-            },
-            {
-                'uid'   : '234623471',
-                'title' : 'Modules',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 3'
-                    }
-                ]
-            },
-            {
-                'uid'   : '234623471',
-                'title' : 'Pages',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 3'
-                    }
-                ]
-            },
-            {
-                'uid'   : '234623471',
-                'title' : 'Modules',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 3'
-                    }
-                ]
-            },
-            {
-                'uid'   : '234623471',
-                'title' : 'Pages',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Page 3'
-                    }
-                ]
-            },
-            {
-                'uid'   : '234623471',
-                'title' : 'Modules',
-                'type'  : 'urlList',
-                'urls'  : [
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 2'
-                    },
-                    {
-                        'url'   : '/#/page',
-                        'title' : 'My Cool Module 3'
-                    }
-                ]
-            }
-        ];
+    $scope.alerts = [
+        {
+            'type' : 'info',
+            'alert' : 'There is a new version of something available'
+        }
+    ];
 
-    angular.forEach($scope.dashboardPanels, function(dashboardPanels) {
-        dashboardPanels.rank = Math.random();
-    });
-  });
+    $scope.dashboardPanels = [
+        {
+            'uid'   : '234623471',
+            'title' : 'Quick Actions',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'New Page +'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'New Module +'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'Promo Scheduler'
+                }
+            ]
+        },
+        {
+            'uid'   : '234623471',
+            'title' : 'Pages',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 2'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 3'
+                }
+            ]
+        },
+        {
+            'uid'   : '234623471',
+            'title' : 'Modules',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module 2'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module 3'
+                }
+            ]
+        },
+        {
+            'uid'   : '234623471',
+            'title' : 'Pages',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 2'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 3'
+                }
+            ]
+        },
+        {
+            'uid'   : '234623471',
+            'title' : 'Modules',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module 2'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Module 3'
+                }
+            ]
+        },
+        {
+            'uid'   : '234623471',
+            'title' : 'Pages',
+            'type'  : 'urlList',
+            'urls'  : [
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 2'
+                },
+                {
+                    'url'   : '/#/page',
+                    'title' : 'My Cool Page 3'
+                }
+            ]
+        }
+    ];
+});
